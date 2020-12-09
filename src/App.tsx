f997e881b44dee602075a7f879be8ad345a8e137
JZachrisson <jesper.zachrisson@gmail.com>
2020-12-09 15:58:31 +0100

    connecting to the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {fetchQuizQuestions} from './API'
+import { useTheme } from 'styled-components';
 import QuestionCard from './components/QuestionCard';
 
+import {Difficulty} from './API'
+
 const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+
+   
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
   const nextQuestion = () => {};
+
+  const TOTAL_QUESTIONS = 10
+
+  console.table(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
 
   return (
     <div className="App">
@@ -16,7 +34,13 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      {/* <QuestionCard questionNr={number + 1} 
+      totalQuestions={TOTAL_QUESTIONS}
+      question={questions[number].question}
+      answers={questions[number].answers}
+      userAnswer={userAnswers ? userAnswers[number] : undefined}
+      callback={checkAnswer}
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 type Props = {
   question: string;
-  answer: string[];
+  answers: string[];
   callback: any;
-  userAnswer: string;
+  userAnswer: any;
   questionNr: number;
   totalQuestions: number;
 };
 
 const QuestionCard: React.FC<Props> = ({
   question,
-  answer,
+  answers,
   callback,
   userAnswer,
   questionNr,
@@ -22,6 +22,16 @@ const QuestionCard: React.FC<Props> = ({
       <p className="number">
         Question: {questionNr} / {totalQuestions}
       </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => {
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}}/>
+            </button>
+          </div>;
+        })}
+      </div>
     </div>
   );
 };
